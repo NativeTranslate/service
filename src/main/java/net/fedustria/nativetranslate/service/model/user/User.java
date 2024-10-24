fc4b1c52c274a08f069ca77e727df60f63521ec9
Florian <mrminecrafttv19@gmail.com>
2024-10-24 07:10:06 +0200

    feat: added avatar

--- src/main/java/net/fedustria/nativetranslate/service/model/user/User.java
+++ src/main/java/net/fedustria/nativetranslate/service/model/user/User.java
@@ -58,6 +58,11 @@ public class User {
     private String role;
 
     /**
+     * The avatar of the user
+     */
+    private String avatar;
+
+    /**
      * The country of the user.
      */
     private String country;
